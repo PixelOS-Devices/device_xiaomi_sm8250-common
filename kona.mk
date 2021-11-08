b85f0cf8b5683e8f14be51946de3175589ca26fb
Devin Moore <devinmoore@google.com>
2021-11-08 17:52:47 +0000

    alioth: Switch to new AIDL IR service
    Test: atest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    Change-Id: I70c54aa8e8962939cc8f4b42fbbe828e9dad7f0d
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kona.mk
+++ kona.mk
@@ -188,8 +188,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Component overrides
 PRODUCT_COPY_FILES += \
