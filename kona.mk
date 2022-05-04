70062390455bdc6d8d0f287b6fe4e7bc20253376
cristianvaz <cristianvaz81993@gmail.com>
2022-05-04 12:00:29 +0200

    sm8250-common: Drop Dolby
    Revert "sm8250-common: Add Moto Dolby"
    
    This reverts commit
    a7605cdece75efa715ca9efb0b5f1bd658f0c54c
    
    Revert "sm8250-common: Turn off Hardware Dolby Acceleration"
    
    This reverts commit 88f69dcb5a75ad400d01c3858dc9d9a919080a19.

--- kona.mk
+++ kona.mk
@@ -183,12 +183,6 @@ PRODUCT_PACKAGES += \
     libadpcmdec \
     sound_trigger.primary.kona:32
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.audio.dolby.ds2.enabled=true \
-    vendor.audio.dolby.ds2.hardbypass=false \
-    persist.vendor.audio_fx.current=dolby \
-    ro.vendor.dolby.dax.version=DS1_2.2.0.0_r1
-
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
@@ -199,10 +193,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
-
-# Audio-Motodolby config
-PRODUCT_COPY_FILES += \
-    device/xiaomi/sm8250-common/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
