7b350890f99a8048d6ea22dc058a68fe56cb8edb
johnmart19 <johnivan19999@gmail.com>
2022-09-23 06:47:56 +0300

    sm8250-common: Sepolicy: Rework thermal part of Sepolicy
    Based on changes:
    UtsavBalar1231/platform_device_xiaomi_alioth@26b8ad18f1a60039198133fb64e92e91c7f4da22
    UtsavBalar1231/platform_device_xiaomi_alioth@051ae1d018d59707d443d2fd8c6c3e3e6154c518
    
    Co-authored-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kona.mk
+++ kona.mk
@@ -484,6 +484,10 @@ PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi
 endif
 
+# Thermal Mi_Thermald
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.sys.thermal.data.path=/data/vendor/thermal/
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
