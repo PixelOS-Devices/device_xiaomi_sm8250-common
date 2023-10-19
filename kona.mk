d3ef35589ba1070f1693e9f03245e208da0e278b
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    sm8250-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kona.mk
+++ kona.mk
@@ -416,7 +416,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Power
 PRODUCT_PACKAGES += \
