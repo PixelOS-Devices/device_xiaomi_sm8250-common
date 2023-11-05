98bdce8d7232f7a475f49f47e18a4e6ea6392d16
NotZeetaa <rodrigo2005contente@gmail.com>
2023-11-05 23:31:19 +0000

    Revert "sm8250-common: Provide android.frameworks.sensorservice@1.0 on vendor"
    This reverts commit 7e90921a55be3ec035a3cde2c72833725cb020c4.
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kona.mk
+++ kona.mk
@@ -183,8 +183,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
-    android.frameworks.sensorservice@1.0.vendor
+    android.hardware.camera.provider@2.4-service_64
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
