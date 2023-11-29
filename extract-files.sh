41fa5610c57888e751034fcda4042187c7a04bd1
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    sm8250-common: Link dolby blobs against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,9 @@ function blob_fixup() {
         vendor/lib64/libril-qc-hal-qmi.so)
             sed -i 's|ro.product.vendor.device|ro.vendor.radio.midevice|g' "${2}"
             ;;
+        vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib64/libdlbdsservice.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
--- kona.mk
+++ kona.mk
@@ -362,6 +362,9 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
