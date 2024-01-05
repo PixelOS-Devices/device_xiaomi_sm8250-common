ed630710d8b46840204180a9aac1237e0c3b2c13
yoriscape <sukhpreetsingh58627@gmail.com>
2024-01-05 14:33:28 +0100

    sm8250-common: Build "android.hardware.power@1.2.vendor"
    in logs
    01-05 04:26:47.636  9317  9317 E vndksupport: Could not load /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so from default namespace: dlopen failed: library "android.hardware.power@1.2.so" not found: needed by /vendor/lib64/hw/android.hardware.gnss@2.1-impl-qti.so in namespace (default)

--- kona.mk
+++ kona.mk
@@ -384,7 +384,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
