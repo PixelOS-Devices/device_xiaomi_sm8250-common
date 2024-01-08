88f92096d79cf1f3df57e9c1882bcf2bb96225e5
johnmart19 <johnivan19999@gmail.com>
2024-01-08 23:12:07 +0200

    sm8250-common: Build VNDK 30, 31, 32, 33 fallback
    - Same as in Miui system_ext:
    com.android.vndk.v30.apex
    com.android.vndk.v31.apex
    com.android.vndk.v32.apex
    
    - Same as in Miui Poco F5 Pro and Mi Pad 6 Android 14:
    com.android.vndk.v30.apex
    com.android.vndk.v31.apex
    com.android.vndk.v32.apex
    com.android.vndk.v33.apex

--- kona.mk
+++ kona.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Target VNDK Fallback version
+PRODUCT_EXTRA_VNDK_VERSIONS := 30 31 32 33
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
