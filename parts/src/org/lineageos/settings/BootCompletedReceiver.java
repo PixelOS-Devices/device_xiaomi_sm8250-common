423e859b38cdf22fb1cf3f8f53a987a168311508
johnmart19 <johnivan19999@gmail.com>
2024-03-16 09:58:25 +0200

    sm8250-common: parts: Import DC Dimming and HBM Settings
    * Implement DC Dimming preference
    * Disable DC Dimming if it's not supported
    * Implement HBM switch
    * Adapt HBM and DC dimming settings to S style
    * Define DC dimming and HBM nodes / keys in one place
    * Declare exported flags in manifest
    * Fix compilation for QPR1
    
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Adhitya Mohan <me@adhityamohan.in>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: alibei <sebastiankotzias83@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,16 +24,30 @@ import android.util.Log;
 
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.utils.FileUtils;
+import android.content.SharedPreferences;
+import androidx.preference.PreferenceManager;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
+    private static final String HBM_ENABLE_KEY = "hbm_mode";
+    private static final String HBM_NODE = "/sys/class/drm/card0-DSI-1/disp_param";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
+        // DC Dimming
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+        boolean hbmEnabled = sharedPrefs.getBoolean(HBM_ENABLE_KEY, false);
+        FileUtils.writeLine(HBM_NODE, hbmEnabled ? "0x10000" : "0xF0000");
     }
 }
