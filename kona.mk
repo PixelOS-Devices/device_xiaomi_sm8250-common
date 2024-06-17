31bb400f9c1437d2fb91da425022eb6ce989a9c4
NotZeetaa <rodrigo2005contente@gmail.com>
2024-06-17 18:40:51 +0100

    sm8250-common: Build Xiaomi Dolby
    * From hardware/xiaomi
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kona.mk
+++ kona.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
