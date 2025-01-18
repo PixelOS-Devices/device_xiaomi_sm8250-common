bcc3cc8ec45663f29796099e03028b24ea09b6d3
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    sm8250-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- kona.mk
+++ kona.mk
@@ -400,8 +400,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8250-common/sm8250-common-vendor.mk)
