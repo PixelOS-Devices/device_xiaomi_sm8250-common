bfd37a1fada83c7bd0fedb29ed97bde5a455a99f
Tkiliay <sackclouths@gmail.com>
2025-04-12 20:02:51 +0800

    sm8250-common: Update blobs from munch OS1.0.10.0.ULMCNXM
    Change-Id: I11f31162fe30d8495e24f3372ebfea3f8fb58d34

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,7 +185,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-09-01
+VENDOR_SECURITY_PATCH := 2025-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
