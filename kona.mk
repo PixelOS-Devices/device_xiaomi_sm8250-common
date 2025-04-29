a53304f03330c72f63b01483c99bf5c339a8d8a6
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm8250-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- kona.mk
+++ kona.mk
@@ -244,6 +244,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
